2aa0aef424c750539e9c1ccf0deccaccceb55fcd
NateUllman <60152037+NateUllman@users.noreply.github.com>
2020-05-18 20:16:00 -0400

    Add files via upload

---
+++ Main Menu/Assets/SpaceSkies Free/Demo/Scripts/copy_cameraZoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class copy_cameraZoom : MonoBehaviour
+{
+    private int zoomIn = 5;
+    private int zoomOut = 5;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            GetComponent<Camera>().fieldOfView -= zoomIn;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            GetComponent<Camera>().fieldOfView += zoomOut;
+        }
+    }
+}
+
+
+
